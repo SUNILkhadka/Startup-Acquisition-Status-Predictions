4016a90402ef49bdadf297649a4648d3dad5162a
SUNILkhadka <doleshorkhad@gmail.com>
2022-05-10 18:46:05 -0400

    flask,flask_api-deployment-completed

--- flask_api.py
+++ flask_api.py
@@ -15,21 +15,35 @@ def home():
 @app.route("/predict",methods=['POST'])
 def predict():
     features = np.array([x for x in request.form.values()])
-    print(features)
-    input_df = pd.DataFrame(features.reshape(1,9),columns=['funding_rounds', 'funding_total_usd','milestones', 
-         'relationships', 'lat', 'lng','activeDays','category_code', 'country_code'])
+    for i,x in enumerate(features):
+        if x == '' or x=='nan' or x=='NaN':
+            features[i] = np.nan
+    input_df = pd.DataFrame(features.reshape(1,8),
+                            columns=['founded_at','funding_rounds', 'funding_total_usd','milestones', 
+                                    'relationships','activeDays','category_code', 'country_code']
+                            )
     output = model.predict(input_df)
-    print(output[0])
+    prob = model.predict_proba(input_df)
     if output[0] == 1:
         output = 'Closed'
     else:
         output = 'Not Closed'
-    return render_template('base.html',prediction_test='The status of the company is : "{}"'.format(output))
+    return render_template('base.html',
+                            prediction_test="The status of the company is : '{}'".format(output),
+                            prediction_prob='[{:.2f}%,{:.2f}%]'.format(prob[0,0]*100,prob[0,1]*100)
+                            )
 
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-    output = prediction[0]
-    return jsonify(output)
+    input_data = pd.read_json(data)
+    prediction = model.predict(input_data)
+    prob = model.predict_proba(input_data)
+    return jsonify(result=int(prediction[0]),
+                    prediction_prob=[float(prob[0,0]),float(prob[0,1])]
+                    )
+    # Another way
+    # output = {'result':int(prediction[0]}
+    # return jsonify(output)
+    
